dc28557fad4c2a0b985e74d5dbdc13e434025499
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-06-04 18:13:44 +0200

    responsive

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
   const addForm = document.getElementById("add-form");
   const addModal = document.getElementById("add-modal");
   const closeModal = document.getElementById("add_modal__close");
+  const productsList = document.getElementById("products-list");
   const addButtons = document.getElementsByClassName("add_button");
 
   //methods
@@ -13,6 +14,23 @@
 
   addForm.addEventListener("submit", function (e) {
     e.preventDefault();
+    const formData = new FormData(e.target);
+    productsList.insertAdjacentHTML(
+      "afterbegin",
+      `
+        <li class="list_item"> 
+          <div class="list_item__thumb">
+            <img src="./images/Cover image1.png" alt="Item Thumbnail" />
+          </div>
+          <div class="list_item__content"> 
+            <h4> ${formData.get("title")} </h4>
+            <p> ${formData.get("description")} </p>
+          </div>
+          <h3 class="list_item__price">20€</h3>
+        </li>
+      `
+    );
+    addModal.classList.remove("add_modal--shown");
     this.reset();
   });
 
@@ -25,4 +43,32 @@
   function showAddModal() {
     addModal.classList.add("add_modal--shown");
   }
+
+  fetch("https://dummyjson.com/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((res) => {
+      res.products.forEach((item) => {
+        productsList.insertAdjacentHTML(
+          "afterbegin",
+          `<li class="list_item">
+          <div class="list_item__thumb">
+              <img src="${item.thumbnail}" />
+          </div>
+          <div class="list_item__content">
+              <h4>${item.title} </h4>
+              <p> ${item.description} </p>
+          </div>
+          <h3 class="list_item__price">${item.price}€</h3>
+      </li>`
+        );
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    .finally(() => {
+      console.log("finally block");
+    });
 })();
